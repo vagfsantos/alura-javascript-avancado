75f2c5336dccabff11c798df12c6334303772452
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-06-19 00:51:51 -0300

    get method

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -3,21 +3,23 @@ class Negociacao{
 		this._data = data;
 		this._quantidade = quantidade;
 		this._valor = valor;
+
+		Object.freeze(this);
 	}
 
-	getVolume(){
+	get volume(){
 		return this._quantidade * this._valor;
 	}
 
-	getData(){
+	get data(){
 		return this._data;
 	}
 
-	getQuantidade(){
+	get quantidade(){
 		return this._quantidade;
 	}
 
-	getValor(){
+	get valor(){
 		return this._valor;
 	}
 }
