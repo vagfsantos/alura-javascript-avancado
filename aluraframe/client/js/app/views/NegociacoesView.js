aeb1dd5fb881e9bd41754b3d16493a801baae92b
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-07-03 23:20:43 -0300

    template string

---
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -0,0 +1,45 @@
+class NegociacoesView
+{
+    constructor(elem){
+        this._elem = elem;
+    }
+
+    _update(model){
+        this._elem.innerHTML = this._template(model);
+    }
+
+    _template(model){
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                        <th>VOLUME</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    ${model.negociacoes.map( n => `
+                        <tr>
+                            <td>${DateHelper.dataParaTexto(n.data)}</td>
+                            <td>${n.quantidade}</td>
+                            <td>${n.valor}</td>
+                            <td>${n.volume}</td>
+                        </tr>
+                    `).join('')}
+                </tbody>
+                
+                <tfoot>
+                    <tr>
+                        <td colspan="3"></td>
+                        <td>
+                            ${model.negociacoes.reduce( (total, n) => total + n.volume, 0.0)}
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        `;
+    }
+}
